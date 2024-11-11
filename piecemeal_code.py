cb24f5e68204fc047fb906d6671c5ee731e0a94a
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-11-11 11:33:13 +0800

    Update piecemeal_code.py

--- piecemeal_code.py
+++ piecemeal_code.py
@@ -1,3 +1,54 @@
+
+
+class PasswordEncrypter:
+    @staticmethod
+    def add_to_16(value):
+        # (Q)!: 2024-11-11，这个的作用是啥？
+        # str 不是 16 的倍数那就补足为 16 的倍数
+        while len(value) % 16 != 0:
+            value += '\0'
+        return str.encode(value)  # 返回bytes
+
+    def __init__(self):
+        # 密钥
+        self._key = "123456"
+
+    def encrypt(self, text: str):
+        """ 生成密文 """
+        # 初始化加密器
+        aes = AES.new(add_to_16(self._key), AES.MODE_ECB)
+        # 进行 aes 加密
+        encrypt_aes = aes.encrypt(add_to_16(text))
+        # 用 base64 转成字符串形式
+        encrypted_text = str(base64.encodebytes(encrypt_aes), encoding="utf-8")
+        # 加密后的字符串会被添加换行符...
+        encrypted_text = encrypted_text.strip()
+        return encrypted_text
+
+    def decrypt(self, ciphertext: str):
+        """ 解开密文 """
+        # 初始化加密器
+        aes = AES.new(add_to_16(self._key), AES.MODE_ECB)
+        # 逆向解密 base64 成 bytes
+        base64_decrypted = base64.decodebytes(ciphertext.encode(encoding="utf-8"))
+        # 执行解密密并转码返回 str
+        decrypted_text = str(aes.decrypt(base64_decrypted), encoding="utf-8")
+        return decrypted_text
+
+def main():
+    encrypter = PasswordEncrypter()
+
+    to_be_encrypted_text = "123456"
+    encrypted_text = encrypter.encrypt(to_be_encrypted_text)
+    decrypted_text = encrypter.decrypt(encrypted_text)
+    print(encrypted_text)
+    print(decrypted_text)
+
+
+if __name__ == '__main__':
+    main()
+    
+# -------------------------------------------------------------------------------------------------------------------- #
 class MailConfig:
     def __init__(self, data, subject, str_to, str_cc, list_email, str_bcc="", attachments: t.List[MIMEBase] = None):
         self.data = data
