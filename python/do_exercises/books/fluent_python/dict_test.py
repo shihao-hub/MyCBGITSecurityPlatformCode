a0f7911843dcee96e4a6e598c3b67e3b441b78de
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-11-12 11:25:04 +0800

    Create dict_test.py

---
+++ python/do_exercises/books/fluent_python/dict_test.py
@@ -0,0 +1,5 @@
+import urllib.parse
+from collections import defaultdict
+from collections import UserDict
+
+print(urllib.parse.unquote("%E5%AE%89%E5%85%A8%E6%B5%8B%E8%AF%95%E7%94%A8%E4%BE%8B%E6%A8%A1%E6%9D%BF.xlsx"))
